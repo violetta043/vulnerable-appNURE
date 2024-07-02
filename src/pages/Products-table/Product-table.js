426af9cf8666f5ef3b71afd9c36dad9f4c8549b6
Violetta286 <sinitsina.viol@gmail.com>
2024-07-02 15:24:14 +0300

    made a modal-window

--- src/pages/Products-table/Product-table.js
+++ src/pages/Products-table/Product-table.js
@@ -7,10 +7,14 @@ import { API_URL } from "../../constants";
 
 const ProductTable = () => {
   const [products, setProducts] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false); 
+ 
 
   useEffect(() => {
-    getProduct();
-  }, []);
+    if(!isLoaded){
+      getProduct();
+    }
+  }, [isLoaded]);
 
   const getProduct = async () => {
     try {
@@ -21,17 +25,34 @@ const ProductTable = () => {
       }
       const data = await response.json();
       setProducts(data);
+      setIsLoaded(true);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
+
+  const deleteProduct = async (id, handleClose) => {
+    try {
+      const response = await fetch(`${API_URL}/posts/${id}`,{
+        method: 'DELETE'
+      });
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      setIsLoaded(false);
+      handleClose(); 
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  
   return (
     <div className="ProductTable">
       <img src={logo_product} className="Product-logo" />
       <div className="Product-button">
         <ProductButton />
       </div>
-      <Table products={products} />
+      <Table products={products} deleteItem={deleteProduct}/>
     </div>
   );
 };
